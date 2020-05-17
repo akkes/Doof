9269acd172bb5687297fa968d2d15a805df66514
Louis Desportes <louis@akkes.fr>
2020-05-17 17:28:07 +0200

    improve display

--- doof/helpers.py
+++ doof/helpers.py
@@ -2,9 +2,9 @@ from doof import model
 
 
 def display_aft(aft: model.ContentNode, indent=0):
-    indentation = "--" * max(0, indent - 1)
+    indentation = "  " * max(0, indent - 1)
     if indent >= 1:
-        indentation += "-> "
+        indentation += "└─ "
     print(indentation + aft.name + ":" + aft.__class__.__name__)
     for child in aft.children:
         display_aft(child, indent=indent + 1)
